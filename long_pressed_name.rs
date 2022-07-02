72f1a6a6ca7c089ec151c9a6265f55f0f34d268d
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-07-02 16:56:22 +0800

    Long Pressed Name

---
+++ long_pressed_name.rs
@@ -0,0 +1,52 @@
+//! <https://leetcode.com/problems/long-pressed-name/>
+pub fn is_long_pressed_name(name: String, typed: String) -> bool {
+    let mut i = 0;
+    let mut j = 0;
+    'outer: while i < name.len() {
+        let ch = name.chars().nth(i).unwrap();
+        let mut repetition = 1;
+        while i < name.len() - 1 && ch == name.chars().nth(i + 1).unwrap() {
+            i += 1;
+            repetition += 1;
+        }
+        let mut matched = 0;
+        loop {
+            if ch
+                == match typed.chars().nth(j) {
+                    Some(ch) => ch,
+                    None => break 'outer,
+                }
+            {
+                matched += 1;
+            } else {
+                break;
+            }
+            j += 1;
+        }
+        if matched < repetition {
+            return false;
+        }
+        i += 1;
+    }
+    i == name.len() - 1
+}
+
+fn main() {
+    assert!(!is_long_pressed_name(String::from("a"), String::from("b")));
+    assert!(is_long_pressed_name(
+        String::from("alex"),
+        String::from("aaleex")
+    ));
+    assert!(is_long_pressed_name(
+        String::from("leelee"),
+        String::from("lleeelee")
+    ));
+    assert!(!is_long_pressed_name(
+        String::from("saeed"),
+        String::from("")
+    ));
+    assert!(!is_long_pressed_name(
+        String::from("saeed"),
+        String::from("ssaaedd")
+    ));
+}
