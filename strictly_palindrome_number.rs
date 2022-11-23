6cac8ac4a274d1b8f6a4ce7eacc86a160a9e7d06
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-11-23 21:46:32 +0800

    Strictly Palindromic Number

---
+++ strictly_palindrome_number.rs
@@ -0,0 +1,9 @@
+//! <https://leetcode.com/problems/strictly-palindrome-number/>
+pub fn is_strictly_palindromic(n: i32) -> bool {
+    false
+}
+
+fn main() {
+    assert!(!is_strictly_palindromic(9));
+    assert!(!is_strictly_palindromic(4));
+}
