98ba44408ea9c4945926304c6ada915a62d318c2
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-03-23 15:45:00 +1100

    Same Tree

---
+++ same_tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file same_tree.cpp
+ * @brief https://leetcode.com/problems/same-tree/
+ * @date 2024-03-23
+ */
+#include <cassert>
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode()
+	: val(0)
+	, left(nullptr)
+	, right(nullptr) {}
+	TreeNode(int x)
+	: val(x)
+	, left(nullptr)
+	, right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right)
+	: val(x)
+	, left(left)
+	, right(right) {}
+};
+
+bool isSameTree(TreeNode *p, TreeNode *q) {
+	if (p == nullptr and q == nullptr)
+		return true;
+	else if (p == nullptr or q == nullptr)
+		return false;
+	if (p->val != q->val)
+		return false;
+	else
+		return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+}
+
+int main() {
+	TreeNode two = TreeNode(2);
+	TreeNode three = TreeNode(3);
+	TreeNode one = TreeNode(1, &two, &three);
+	assert(isSameTree(&one, &one));
+	assert(not isSameTree(&one, &two));
+	assert(not isSameTree(&two, &three));
+	assert(isSameTree(nullptr, nullptr));
+	assert(not isSameTree(&one, nullptr));
+	TreeNode two_copy = TreeNode(2);
+	TreeNode three_copy = TreeNode(3);
+	TreeNode one_copy = TreeNode(1, &two_copy, &three_copy);
+	assert(isSameTree(&one, &one_copy));
+	return 0;
+}
