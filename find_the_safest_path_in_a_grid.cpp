24376da146474bada3a18096f892ba7edc667781
Kaiqi Liang <kaiqi.liang@outlook.com>
2024-04-05 07:29:25 +1100

    Max Area of Island

--- find_the_safest_path_in_a_grid.cpp
+++ find_the_safest_path_in_a_grid.cpp
@@ -10,34 +10,23 @@
 #include <queue>
 #include <unordered_set>
 #include <vector>
+
+#include "grid.hpp"
 using namespace std;
 
-constexpr array<pair<size_t, size_t>, 4> neighbours = {{
-    {0, 1},
-    {0, -1},
-    {1, 0},
-    {-1, 0},
-}};
-
-struct pair_hash {
-	inline size_t operator()(const pair<size_t, size_t> &v) const {
-		return v.first * 31 + v.second;
-	}
-};
-
-bool is_safe(vector<vector<size_t>> const &dist, size_t safe_dist, size_t n) {
+bool is_safe(vector<vector<int>> const &dist, int safe_dist, int n) {
 	if (dist[0][0] < safe_dist) return false;
-	queue<pair<size_t, size_t>> q{{{0, 0}}};
-	unordered_set<pair<size_t, size_t>, pair_hash> visited{{0, 0}};
+	queue<Point> q{{{0, 0}}};
+	unordered_set<Point> visited{{0, 0}};
 	while (not q.empty()) {
 		auto [curr_row, curr_col] = q.front();
 		q.pop();
 		if (curr_row == n - 1 and curr_col == n - 1) {
 			return true;
 		}
-		for (auto &&[i, j] : neighbours) {
-			size_t new_row = curr_row + i;
-			size_t new_col = curr_col + j;
+		for (auto &&[i, j] : NEIGHBOURS) {
+			int new_row = curr_row + i;
+			int new_col = curr_col + j;
 			if (new_row >= 0 and new_row < n and new_col >= 0 and
 			    new_col < n and not visited.contains({new_row, new_col}) and
 			    dist[new_row][new_col] >= safe_dist)
@@ -50,25 +39,25 @@ bool is_safe(vector<vector<size_t>> const &dist, size_t safe_dist, size_t n) {
 	return false;
 }
 
-size_t maximumSafenessFactor(vector<vector<size_t>> const &grid) {
-	size_t const n = grid.size();
-	queue<pair<size_t, size_t>> q;
-	unordered_set<pair<size_t, size_t>, pair_hash> visited;
-	for (size_t i = 0; i < n; ++i) {
-		for (size_t j = 0; j < n; ++j) {
+int maximumSafenessFactor(vector<vector<int>> const &grid) {
+	int const n = grid.size();
+	queue<Point> q;
+	unordered_set<Point> visited;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 			if (grid[i][j]) {
 				q.emplace(i, j);
 				visited.emplace(i, j);
 			}
 		}
 	}
-	vector<vector<size_t>> dist(n, vector<size_t>(n, 0));
+	vector<vector<int>> dist(n, vector<int>(n, 0));
 	while (not q.empty()) {
 		auto [curr_row, curr_col] = q.front();
 		q.pop();
-		for (auto &&[i, j] : neighbours) {
-			size_t new_row = curr_row + i;
-			size_t new_col = curr_col + j;
+		for (auto &&[i, j] : NEIGHBOURS) {
+			int new_row = curr_row + i;
+			int new_col = curr_col + j;
 			if (new_row >= 0 and new_row < n and new_col >= 0 and
 			    new_col < n and not visited.contains({new_row, new_col}))
 			{
@@ -78,17 +67,17 @@ size_t maximumSafenessFactor(vector<vector<size_t>> const &grid) {
 			}
 		}
 	}
-	size_t l = 2 * n;
-	size_t h = 0;
-	for (size_t i = 0; i < n; ++i) {
-		for (size_t j = 0; j < n; ++j) {
-            l = min(l, dist[i][j]);
-            h = max(h, dist[i][j]);
+	int l = 2 * n;
+	int h = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			l = min(l, dist[i][j]);
+			h = max(h, dist[i][j]);
 		}
 	}
-	size_t ans = l;
+	int ans = l;
 	while (l <= h) {
-		size_t m = (l + h) / 2;
+		int m = (l + h) / 2;
 		if (is_safe(dist, m, n)) {
 			ans = m;
 			l = m + 1;
---
+++ grid.hpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <functional>
+using namespace std;
+
+using Point = pair<int, int>;
+
+constexpr array<Point, 4> NEIGHBOURS = {{
+    {0, 1},
+    {0, -1},
+    {1, 0},
+    {-1, 0},
+}};
+
+namespace std {
+	template<>
+	struct hash<Point> {
+		int operator()(const Point &point) const noexcept {
+			return point.first * 31 + point.second;
+		}
+	};
+} // namespace std
