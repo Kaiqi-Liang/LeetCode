3b0f25bba359dd604e367563f846eeefba66ee99
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-04-25 10:34:09 +1000

    Maximal Square

--- n-th_trigonacci_number.swift
+++ n-th_trigonacci_number.swift
@@ -1,17 +1,17 @@
 /// https://leetcode.com/problems/n-th-tribonacci-number/
 class Solution {
-	private var cache = Array(repeating: -1, count: 38)
-	init() {
-		cache[0] = 0
-		cache[1] = 1
-		cache[2] = 1
-	}
-	func tribonacci(_ n: Int) -> Int {
-		if cache[n] == -1 {
-			cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
-		}
-		return cache[n] 
-	}
+    private var cache = Array(repeating: -1, count: 38)
+    init() {
+        cache[0] = 0
+        cache[1] = 1
+        cache[2] = 1
+    }
+    func tribonacci(_ n: Int) -> Int {
+        if cache[n] == -1 {
+            cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+        }
+        return cache[n] 
+    }
 }
 
 assert(Solution().tribonacci(2) == 1)
