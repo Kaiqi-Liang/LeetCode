54e9f63a434d9ac9af4be62c3e6af93305866c53
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-04-27 08:22:47 +1000

    Minimum Falling Path Sum II

--- longest_ideal_subsequence.swift
+++ longest_ideal_subsequence.swift
@@ -2,19 +2,19 @@
 class Solution {
     func longestIdealString(_ s: String, _ k: Int) -> Int {
         let numLowercaseLetters = 26
-        var dp = Array(repeating: 0, count: numLowercaseLetters)
+        var opt = Array(repeating: 0, count: numLowercaseLetters)
         for ch in s {
             var maxPrev = 0
             let index = Int(ch.asciiValue! - Character("a").asciiValue!)
             for i in -k...k {
                 let prev = index + i
-                if prev >= 0 && prev < numLowercaseLetters && dp[prev] > 0 {
-                    maxPrev = max(maxPrev, dp[prev])
+                if prev >= 0 && prev < numLowercaseLetters && opt[prev] > 0 {
+                    maxPrev = max(maxPrev, opt[prev])
                 }
             }
-            dp[index] = maxPrev + 1
+            opt[index] = maxPrev + 1
         }
-        return dp.max()!
+        return opt.max()!
     }
 }
 
