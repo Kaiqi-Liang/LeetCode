ab420af4ddf5ee87fbd6669af94cbb4a0a421b76
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-04-28 17:58:39 +1000

    Remove All Occurrences of a Substring

---
+++ remove_all_occurrences_of_a_substring.swift
@@ -0,0 +1,14 @@
+/// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+class Solution {
+    func removeOccurrences(_ s: String, _ part: String) -> String {
+        var s = s
+        while let i = s.firstRange(of: part) {
+            s.removeSubrange(i)
+        }
+        return s
+ 
+    }
+}
+
+assert(Solution().removeOccurrences("daabcbaabcbc", "abc") == "dab")
+assert(Solution().removeOccurrences("axxxxyyyyb", "xy") == "ab")
