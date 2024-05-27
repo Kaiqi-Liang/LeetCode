95d58c3a13d434854a8b30d1011492eb1d976769
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-05-27 10:52:55 +1000

    Special Array With X Elements Greater Than or Equal X

---
+++ LeetCode/SpecialArrayWithXElementsGreaterThanOrEqualX.swift
@@ -0,0 +1,20 @@
+func specialArray(_ nums: [Int]) -> Int {
+    for x in 0...nums.count {
+        var numGreaterThanOrEqualToX = 0
+        for num in nums {
+            if num >= x {
+                numGreaterThanOrEqualToX += 1
+            }
+        }
+        if numGreaterThanOrEqualToX == x {
+            return x
+        }
+    }
+    return -1
+}
+
+func testSpecialArrayWithXElementsGreaterThanOrEqualX() {
+    assert(specialArray([3, 5]) == 2)
+    assert(specialArray([0, 0]) == -1)
+    assert(specialArray([0, 4, 3, 0, 4]) == 3)
+}
