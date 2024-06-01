5e3026ed5b09a89d5ff67a075042543ef5ddd388
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-06-01 17:30:22 +1000

    Score of a String

---
+++ LeetCode/ScoreOfAString.swift
@@ -0,0 +1,14 @@
+func scoreOfString(_ s: String) -> Int {
+    var score = 0
+    for i in 1..<s.count {
+        let a = Int(s[String.Index(utf16Offset: i, in: s)].asciiValue!)
+        let b = Int(s[String.Index(utf16Offset: i - 1, in: s)].asciiValue!)
+        score += abs(a - b)
+    }
+    return score
+}
+
+func testScoreOfAString() {
+    assert(scoreOfString("hello") == 13)
+    assert(scoreOfString("zaz") == 50)
+}
