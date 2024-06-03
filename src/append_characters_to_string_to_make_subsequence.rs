5031d32f7b73f2d653ef1e80237b61c907e6eaa6
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-06-03 15:25:02 +1000

    Append Characters to String to Make Subsequence

---
+++ src/append_characters_to_string_to_make_subsequence.rs
@@ -0,0 +1,69 @@
+pub fn append_characters(s: String, t: String) -> i32 {
+    let mut i = 0;
+    let mut j = 0;
+    let s = Vec::from(s);
+    let t = Vec::from(t);
+    while i < s.len() && j < t.len() {
+        if s[i] == t[j] {
+            i += 1;
+            j += 1;
+        } else {
+            i += 1;
+        }
+    }
+    (t.len() - j) as _
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn append_subsequence() {
+        assert_eq!(
+            append_characters(String::from("coaching"), String::from("coding")),
+            4
+        );
+    }
+
+    #[test]
+    fn already_a_subsequence() {
+        assert_eq!(
+            append_characters(String::from("coachding"), String::from("coding")),
+            0
+        );
+        assert_eq!(
+            append_characters(String::from("zcaobdcidnegf"), String::from("coding")),
+            0
+        );
+    }
+
+    #[test]
+    fn append_none() {
+        assert_eq!(
+            append_characters(String::from("abcde"), String::from("a")),
+            0
+        );
+    }
+
+    #[test]
+    fn append_all() {
+        assert_eq!(
+            append_characters(String::from("z"), String::from("abcde")),
+            5
+        );
+    }
+
+    #[test]
+    fn empty() {
+        assert_eq!(
+            append_characters(String::from(""), String::from("abcde")),
+            5
+        );
+        assert_eq!(
+            append_characters(String::from("abcde"), String::from("")),
+            0
+        );
+        assert_eq!(append_characters(String::from(""), String::from("")), 0);
+    }
+}
