3ccefb2e3f3cbab49152efe09d46db4bd72a3e58
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-08-06 19:42:12 +1000

    Minimum Number of Pushes to Type Word II

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,7 @@ mod merge_two_sorted_list;
 mod minimum_cost_to_hire_k_workers;
 mod minimum_deletions_to_make_string_balanced;
 mod minimum_number_of_moves_to_seat_everyone;
+mod minimum_number_of_pushes_to_type_word_ii;
 mod number_of_senior_citizens;
 mod pass_the_pillow;
 mod relative_ranks;
