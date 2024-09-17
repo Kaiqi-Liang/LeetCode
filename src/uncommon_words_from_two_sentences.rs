018a49ccf7a49a2df441f1c408230b960c1d7ef8
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-09-17 10:34:47 +1000

    Uncommon Words from Two Sentences

---
+++ src/uncommon_words_from_two_sentences.rs
@@ -0,0 +1,38 @@
+//! <https://leetcode.com/problems/uncommon-words-from-two-sentences/>
+use std::collections::HashMap;
+pub fn uncommon_from_sentences(s1: String, s2: String) -> Vec<String> {
+    counter!(s1.split_whitespace().chain(s2.split_whitespace()))
+        .into_iter()
+        .filter_map(|(word, appearances)| {
+            if appearances == 1 {
+                Some(word.to_string())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn common_words_from_both_sentences() {
+        assert_eq!(
+            uncommon_from_sentences(
+                String::from("this apple is sweet"),
+                String::from("this apple is sour"),
+            ),
+            vec![String::from("sweet"), String::from("sour")],
+        );
+    }
+
+    #[test]
+    fn common_words_from_first_sentence() {
+        assert_eq!(
+            uncommon_from_sentences(String::from("apple apple"), String::from("banana")),
+            vec![String::from("banana")],
+        );
+    }
+}
