3796f5b216cc5699ac1556ca998e97999f96dae4
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-12-07 08:07:39 +1100

    Check If a Word Occurs As a Prefix of Any Word in a Sentence

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ mod average_waiting_time;
 #[cfg(test)]
 mod binary_tree_postorder_traversal;
 #[cfg(test)]
+mod check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence;
+#[cfg(test)]
 mod check_if_n_and_its_double_exist;
 #[cfg(test)]
 mod combination_sum;
