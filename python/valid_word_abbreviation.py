87ae8eb809b655d50a64bea1065295c804028acf
Kaiqi Liang <kaiqi.liang@bytedance.com>
2024-12-14 17:54:33 +1100

    Valid Word Abbreviation

---
+++ python/valid_word_abbreviation.py
@@ -0,0 +1,18 @@
+def check_abbr(word: str, abbr: str) -> bool:
+    curr_count = ""
+    i = 0
+    for j in range(len(abbr)):
+        if abbr[j].isdigit():
+            if not curr_count and abbr[j] == "0":
+                return False
+            curr_count += abbr[j]
+            continue
+        elif curr_count:
+            i += int(curr_count)
+            if i >= len(word):
+                return False
+            curr_count = ""
+        if word[i] != abbr[j]:
+            return False
+        i += 1
+    return i == len(word)
